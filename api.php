0f64e17a35c4bc5093e4d0493d44a50dd305b1c8
AdrianFillippo <adrifillip2@gmail.com>
2022-05-03 18:49:22 -0500

    Ciclo Foreach

--- api.php
+++ api.php
@@ -1,14 +1,24 @@
 <?php
-$_DATA = (object) json_decode(file_get_contents("php://input"));
-    if($_DATA->opcion == "iniciarCiclo"){
-        for ($i=$_DATA->inicio; $i < $_DATA->final; $i+=$_DATA->avance) { 
-            echo "ciclo #$i Incremento <br>";
-        }
-    }else if($_DATA->opcion == "iniciarCiclo2"){
-        for ($i=$_DATA->final; $i > $_DATA->inicio; $i-=$_DATA->avance) { 
-            echo "ciclo #$i Decremento <br>";
-        }
-    }
-    
+   $_DATA = (object) json_decode(file_get_contents("php://input"));
+   $lista = (array) [
+       "miguel",
+       "nombre" => (string) "Juan",
+       "Jose",
+       "Andres",
+       "Lista" => (array) ["Carlos"]
+   ];
+   foreach ($lista as $key => $value) {
+       echo("Llave");
+       print_r($key);
+       echo ",<br>";
+       echo("Valor");
+       print_r($value);
+       echo ",<br>";
+   }
+   //for ($i=0; $i < count($lista); $i++) { 
+       //if(array_key_exists($i, $lista)){
+           //print_r($lista[$i]);
+       //}
+   //}
       
 ?>
--- main.js
+++ main.js
@@ -1,26 +1,8 @@
-let w = new Worker("formulario.js");
-let form = document.querySelector("#ciclo");
-form.addEventListener("submit", (e)=>{
-    let input = Array.from(e.target);
-    let obj = {
-        url: form.action,
-        data:{
-            method: form.method,
-            body: {
-                opcion: e.submitter.dataset.boton
-            }
-        }
-    };
-    for(let [id, value] of Object.entries(input)){
-        if (value.id){
-            obj.data.body[value.id] = Number(value.value);
-        }
-    }
-    w.postMessage(obj);
-    e.preventDefault();
-})
-w.addEventListener("message", (e)=>{
-    document.querySelector(".mensaje").insertAdjacentHTML("beforeend", e.data);  
-
-})
+//let form = document.querySelector("#ciclo");
+let enviar = async()=>{
+    let peticion = await fetch("api.php");
+    let data = await peticion.text();
+    document.querySelector(".mensaje").insertAdjacentHTML("beforebegin", data);
+}
+enviar();
 
