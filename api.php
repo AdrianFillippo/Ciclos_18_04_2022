6903da170617f955693a6b40872163bee65e61c1
AdrianFillippo <adrifillip2@gmail.com>
2022-05-03 14:04:59 -0500

    Ciclo For

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+    for ($i=0; $i < 10; $i++) { 
+        echo "ciclo #$i Incremento <br>";
+    }
+    echo "<hr>";
+    for ($i=9; $i >= 0 ; $i--) { 
+        echo "ciclo #$i Decremento <br>";
+    }
+    //$i=1;
+    //for($i; true;){   
+    //    if($i<10){
+    //        echo "ciclo #$i <br>";
+    //    }else{
+    //        break;
+    //    }
+    //    $i++;
+    //}    
+?>
